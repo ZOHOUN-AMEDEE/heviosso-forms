38dcf88e2b1113a52b8ad629734da10affabfdd2
ZOHOUN-AMEDEE <136507229+ZOHOUN-AMEDEE@users.noreply.github.com>
2025-12-12 13:06:51 +0100

    Fix import path for HeviossoSurvey component

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HeviossoSurvey from './App.js'
+import HeviossoSurvey from 'App.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HeviossoSurvey />
   </React.StrictMode>,
+
 )
